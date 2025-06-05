8057b53e9342877dee1a8121ad0aba25d4bac00a
pixelsailor <smoov.b@gmail.com>
2025-06-05 01:22:27 -0500

    feat(HULA-5): add scrolling support for playlist and lyrics cards

--- apps/svelte-frontend/src/routes/LyricsCard.svelte
+++ apps/svelte-frontend/src/routes/LyricsCard.svelte
@@ -7,7 +7,7 @@
   let vp: { w: () => number, h: () => number, offset: () => number, isDesktop: () => boolean} = getContext('viewport');
 </script>
 
-<article class="card bg-black rounded-t-xl shadow text-white grow w-full" transition:fly={{y: vp.h()}}>
+<article class="card bg-black rounded-t-xl shadow text-white grow w-full flex flex-col overflow-hidden" transition:fly={{y: vp.h()}}>
   <button class="card__handle w-full h-5" onclick={() => showLyrics = !showLyrics}>
     <span
       class="card__handle__line block mx-auto bg-white my-2 w-8 h-1 rounded opacity-50 overflow-hidden"
@@ -15,7 +15,7 @@
     ></span>
     <span class="sr-only">Close lyrics</span>
   </button>
-  <main class="lyrics__content p-4">
-    <pre class="whitespace-pre-wrap font-sans">{lyrics}</pre>
+  <main class="lyrics__content h-full mt-2 p-4 overflow-y-auto overscroll-contain">
+    <pre class="whitespace-pre-wrap font-sans">{@html lyrics}</pre>
   </main>
 </article>
--- apps/svelte-frontend/src/routes/PlaylistCard.svelte
+++ apps/svelte-frontend/src/routes/PlaylistCard.svelte
@@ -20,7 +20,7 @@
   let vp: { w: () => number, h: () => number, offset: () => number, isDesktop: () => boolean} = getContext('viewport');
 </script>
 
-<article class="card bg-black rounded-t-xl shadow text-white grow w-full" transition:fly={{y: vp.h()}}>
+<article class="card bg-black rounded-t-xl shadow text-white grow w-full flex flex-col overflow-hidden" transition:fly={{y: vp.h()}}>
   <button class="card__handle w-full h-5" onclick={() => showPlaylist = !showPlaylist}>
     <span
       class="card__handle__line block mx-auto bg-white my-2 w-8 h-1 rounded opacity-50 overflow-hidden"
@@ -32,7 +32,7 @@
     <h1 class="playlist__title">{playlist.title}</h1>
     <p class="playlist__artist">{playlist.artistName}</p>
   </header>
-  <main class="playlist__content my-4">
+  <main class="playlist__content h-full pb-4 overflow-y-auto overscroll-contain">
     <ul class="playlist__tracks">
       {#each playlist.tracks as track, index}
         <li class="playlist__track flex items-center py-{LIST_ITEM_PADDING} pl-4 pr-2 gap-4">
@@ -41,7 +41,7 @@
             <p class="playlist__track__name">{track.title}</p>
           </div>
           {#if track.downloadable}
-            <IconButton onclick={() => {}}>
+            <IconButton onclick={() => {}} label="Download">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="download-icon">
                 <path d="M12,1C6.878,1,1,2.25,1,12c0,7.71,3.29,11,11,11,5.122,0,11-1.25,11-11,0-7.71-3.29-11-11-11Zm0,21c-7.103,0-10-2.897-10-10S4.897,2,12,2s10,2.897,10,10-2.897,10-10,10Z"/>
                 <g>
@@ -57,7 +57,7 @@
             </video>
           {:else}
           {/if} -->
-          <IconButton onclick={() => selectSongHandler(track)}>
+          <IconButton onclick={() => selectSongHandler(track)} label="Play">
             <svg class="play-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor">
               <path d="m13.249 8.897c-2.553-1.531-4.097-1.873-4.161-1.886-.254-.055-.502.092-.58.335-.021.064-.508 1.61-.508 4.653s.486 4.589.508 4.653c.078.245.335.39.58.335.064-.014 1.608-.355 4.161-1.886 2.447-1.467 3.514-2.644 3.627-2.772.165-.189.165-.471 0-.66-.113-.129-1.18-1.305-3.627-2.772zm-.514 5.348c-1.603.961-2.783 1.426-3.393 1.629-.136-.644-.343-1.941-.343-3.874s.207-3.23.343-3.874c.609.203 1.79.668 3.393 1.629 1.624.974 2.6 1.807 3.062 2.245-.463.438-1.438 1.271-3.062 2.245zm-.735-13.245c-7.813 0-11 3.187-11 11s3.187 11 11 11 11-3.187 11-11-3.187-11-11-11zm0 21c-7.196 0-10-2.804-10-10s2.804-10 10-10 10 2.804 10 10-2.804 10-10 10z"/>
             </svg>
