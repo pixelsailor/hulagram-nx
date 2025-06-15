91097ae1064d224041e4b1fa6fa669b9232b3f48
pixelsailor <smoov.b@gmail.com>
2025-06-15 13:07:47 -0500

    chore: add vercel config

--- apps/svelte-frontend/svelte.config.js
+++ apps/svelte-frontend/svelte.config.js
@@ -1,9 +1,14 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter({
+		// fallback: 'index.html'
+		runtime: 'edge',
+		regions: ['sfo1']
+	}) }
 };
 
 export default config;
